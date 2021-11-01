52619a08d737c32f0b44448edeb0622c89194af6
cwpark <cwpark@bbros.kr>
2021-11-01 19:55:30 +0900

    State hoisting

--- week 1-Jetpack Compose basics/ComposeBasic/app/src/main/java/com/codelab/composebasic/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeBasic/app/src/main/java/com/codelab/composebasic/MainActivity.kt
@@ -3,12 +3,10 @@ package com.codelab.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,14 +24,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun OnBoardingScreen(onNextClicked: () -> Unit) {
+    Surface() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("This is OnBoarding Screen")
+            Button(
+                modifier = Modifier.padding(15.dp),
+                onClick = onNextClicked) {
+                Text(text = "Next")
+            }
+        }
+    }
+}
+
+@Composable
 private fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Greetings("Android", "Compose", "World")
+        var shouldShowOnBoarding by remember { mutableStateOf(true)}
+        if(shouldShowOnBoarding) {
+            OnBoardingScreen() {
+                shouldShowOnBoarding = false
+            }
+        } else {
+            Greetings("Android", "Compose", "World")
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false)}
+    val extraPadding = if(expanded.value) 40.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier
@@ -41,14 +66,18 @@ fun Greeting(name: String) {
             .padding(vertical = 2.dp, horizontal = 4.dp)
     ) {
         Row(
-            modifier = Modifier.padding(20.dp, 20.dp)
+            modifier = Modifier.padding(20.dp)
         ) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello")
                 Text(text = "$name")
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "$name")
+            OutlinedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(text = if(expanded.value) "show less" else "show more")
             }
         }
     }
@@ -66,7 +95,7 @@ fun Greetings(vararg names: String) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 240)
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
 fun DefaultPreview() {
     ComposeBasicTheme {
