65a19302266088f62a2585fc0a1a8c44e1c62905
cwpark <cwpark@bbros.kr>
2021-11-01 20:04:54 +0900

    Persisting state

--- week 1-Jetpack Compose basics/ComposeBasic/app/src/main/java/com/codelab/composebasic/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeBasic/app/src/main/java/com/codelab/composebasic/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +47,7 @@ fun OnBoardingScreen(onNextClicked: () -> Unit) {
 @Composable
 private fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        var shouldShowOnBoarding by remember { mutableStateOf(true)}
+        var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true)}
         if(shouldShowOnBoarding) {
             OnBoardingScreen() {
                 shouldShowOnBoarding = false
@@ -59,7 +60,7 @@ private fun MyApp() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false)}
+    val expanded = rememberSaveable { mutableStateOf(false)}
     val extraPadding = if(expanded.value) 40.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
