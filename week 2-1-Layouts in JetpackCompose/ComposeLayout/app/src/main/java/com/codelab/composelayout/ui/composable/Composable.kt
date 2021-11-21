deafdf90f96b916b635188f6916214e93f44c8f9
cwpark <cwpark@bbros.kr>
2021-11-21 15:49:11 +0900

    Constraint Layout

--- week 2-1-Layouts in JetpackCompose/ComposeLayout/app/src/main/java/com/codelab/composelayout/ui/composable/Composable.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayout/app/src/main/java/com/codelab/composelayout/ui/composable/Composable.kt
@@ -1,6 +1,8 @@
 package com.codelab.composelayout.ui.composable
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -16,9 +18,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 import coil.compose.rememberImagePainter
 import com.codelab.composelayout.ui.theme.ComposeLayoutTheme
 import kotlinx.coroutines.CoroutineScope
@@ -193,6 +200,97 @@ fun Chip(
              Spacer(modifier = Modifier.width(4.dp))
              Text(text)
          }
+    }
+}
+
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button1, button2, text) = createRefs()
+
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }) {
+            Text(text = "Button 1")
+        }
+
+        Text(text = "Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 10.dp)
+            centerHorizontallyTo(parent)
+        })
+
+        val barrrier = createEndBarrier(button1, text)
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier.constrainAs(button2) {
+                top.linkTo(button1.top)
+                start.linkTo(barrrier)
+            }) {
+            Text(text = "Button 2")
+        }
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text(
+            text = "This is a very very very very very very very very very very very long text",
+            Modifier.constrainAs(text) {
+                linkTo(start = guideline, end = parent.end)
+                width = Dimension.preferredWrapContent
+            }
+        )
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if(maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /*TODO*/ },
+                Modifier.layoutId("button")
+            ) {
+                Text(text = "button")
+            }
+
+            Text("text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+
+        constrain(text) {
+            top.linkTo(button.bottom, margin = margin)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintPreView() {
+    ComposeLayoutTheme {
+        DecoupledConstraintLayout()
     }
 }
 
