69abdb7cf79fe18821b6af9666f9813e44a1b595
cwpark <cwpark@bbros.kr>
2021-12-04 15:36:32 +0900

    Extract finished NavHost

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,25 +19,16 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navDeepLink
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
 import com.example.compose.rally.ui.accounts.SingleAccountBody
@@ -78,59 +69,10 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            NavHost(
-                navController = navController,
-                startDestination = RallyScreen.Overview.name,
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                val accountsName = RallyScreen.Accounts.name
-
-                composable(RallyScreen.Overview.name) {
-//                    Text(text =RallyScreen.Overview.name)
-                    OverviewBody(
-                        onClickSeeAllAccounts = {
-                            navController.navigate(RallyScreen.Accounts.name)
-                        },
-                        onClickSeeAllBills = {
-                            navController.navigate(RallyScreen.Bills.name)
-                        },
-                        onAccountClick = {
-//                            navController.navigate("$accountsName/$it")
-                            navController.navigateToSingleAccount(it)
-                        }
-                    )
-                }
-
-                composable(accountsName) {
-//                    Text(text =RallyScreen.Accounts.name)
-                    AccountsBody(
-                        accounts = UserData.accounts,
-                        onAccountClick = {
-//                            navController.navigate("$accountsName/$it")
-                            navController.navigateToSingleAccount(it)
-                        }
-                    )
-                }
-
-                composable(RallyScreen.Bills.name) {
-//                    Text(text =RallyScreen.Bills.name)
-                    BillsBody(bills = UserData.bills)
-                }
-
-                composable(
-                    route = "$accountsName/{name}",
-                    arguments = listOf(
-                        navArgument("name") {
-                            type = NavType.StringType
-                        }
-                    ),
-                    deepLinks = listOf(navDeepLink {
-                        uriPattern = "rally://$accountsName/{name}"
-                    })
-                ) {
-                    SingleAccountBody(account = UserData.getAccount(it.arguments?.getString("name")))
-                }
-            }
+                RallyNaviHost(
+                    navController = navController,
+                    modifier = Modifier.padding(innerPadding)
+                )
 
 //            Box(Modifier.padding(innerPadding)) {
 //                currentScreen.content(
@@ -139,6 +81,66 @@ fun RallyApp() {
 //                    }
 //                )
 //            }
+        }
+    }
+}
+
+@Composable
+private fun RallyNaviHost(
+    navController: NavHostController,
+    modifier: Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RallyScreen.Overview.name,
+        modifier = modifier
+    ) {
+        val accountsName = RallyScreen.Accounts.name
+
+        composable(RallyScreen.Overview.name) {
+//                    Text(text =RallyScreen.Overview.name)
+            OverviewBody(
+                onClickSeeAllAccounts = {
+                    navController.navigate(RallyScreen.Accounts.name)
+                },
+                onClickSeeAllBills = {
+                    navController.navigate(RallyScreen.Bills.name)
+                },
+                onAccountClick = {
+//                            navController.navigate("$accountsName/$it")
+                    navController.navigateToSingleAccount(it)
+                }
+            )
+        }
+
+        composable(accountsName) {
+//                    Text(text =RallyScreen.Accounts.name)
+            AccountsBody(
+                accounts = UserData.accounts,
+                onAccountClick = {
+//                            navController.navigate("$accountsName/$it")
+                    navController.navigateToSingleAccount(it)
+                }
+            )
+        }
+
+        composable(RallyScreen.Bills.name) {
+//                    Text(text =RallyScreen.Bills.name)
+            BillsBody(bills = UserData.bills)
+        }
+
+        composable(
+            route = "$accountsName/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                }
+            ),
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "rally://$accountsName/{name}"
+            })
+        ) {
+            SingleAccountBody(account = UserData.getAccount(it.arguments?.getString("name")))
         }
     }
 }
