2ee9fb56a2d0916b78326fa6bb3b5cba8e3b0c8d
cwpark <cwpark@bbros.kr>
2021-12-05 11:52:57 +0900

    Merged and unmerged Semantics trees

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,15 +1,13 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.*
 
 class TopAppBarTest {
     @get:Rule
@@ -38,10 +36,17 @@ class TopAppBarTest {
 
     @Test
     fun rallyTopAppBarTest_currentLabelExists() {
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+//        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
         composeTestRule
 //            .onNodeWithText(RallyScreen.Accounts.name.uppercase(Locale.getDefault()))
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+//            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                matcher = hasText(RallyScreen.Accounts.name.uppercase(Locale.getDefault()))
+                                    // 현재 context에서는 상위 노드의 contentDescription을 확인하지 않아도 테스트 통과가 된다.
+//                                and hasParent(hasContentDescription(RallyScreen.Accounts.name))
+                ,useUnmergedTree = true
+            )
             .assertExists()
     }
 }
