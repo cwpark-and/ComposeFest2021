3c3af21ae13bfdad9faae3aa0d026489c6dc1bd5
cwpark <cwpark@bbros.kr>
2021-12-05 15:20:08 +0900

    Creating a Composable out of Xml

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,59 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
+
+@Composable
+fun PlantName(name: String) {
+//    <TextView-->
+//        android:id="@+id/plant_detail_name"
+//        android:layout_width="0dp"
+//        android:layout_height="wrap_content"
+//        android:layout_marginStart="@dimen/margin_small"
+//        android:layout_marginEnd="@dimen/margin_small"
+//        android:gravity="center_horizontal"
+//        android:text="@{viewModel.plant.name}"
+//        android:textAppearance="?attr/textAppearanceHeadline5"
+//        app:layout_constraintEnd_toEndOf="parent"
+//        app:layout_constraintStart_toStartOf="parent"
+//        app:layout_constraintTop_toTopOf="parent"
+//        tools:text="Apple" />
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5, // android:textAppearance="?attr/textAppearanceHeadline5
+        modifier = Modifier
+            // android:layout_width="match_parent"
+            .fillMaxWidth()
+            // android:layout_marginStart="@dimen/margin_small"
+            // android:layout_marginEnd="@dimen/margin_small"
+            .padding(
+                horizontal = dimensionResource(id = R.dimen.margin_small)
+            )
+            // android:gravity="center_horizontal"
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
 
 @Composable
 fun PlantDetailDescription() {
     Surface {
-        Text("Hello Compose")
+        PlantName("Apple")
     }
+}
+
+@Composable
+@Preview
+fun PlantDetailDescriptionPreview() {
+    PlantDetailDescription()
 }
